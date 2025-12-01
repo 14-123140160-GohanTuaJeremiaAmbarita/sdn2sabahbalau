f7c52737f7192ca42c6df72c83f62c19e7f05f35
14_12310160_Gohan Tua Jeremia Ambarita <gohan.123140160@student.itera.ac.id>
2025-12-01 21:50:26 +0700

    commit update4

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function POST(req: Request) {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+  const supabase = createClient(supabaseUrl, supabaseKey);
   try {
     const body = await req.json();
     const username = (body.username || "").trim();
